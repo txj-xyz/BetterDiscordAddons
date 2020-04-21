ce2d293097e431a012c83a96f658628cbd861035
0x0b6e48 <jfinelli3@gmail.com>
2020-04-21 15:01:25 -0600

    Simplified Loading

--- plugins/RemovePIPVideo.plugin.js
+++ plugins/RemovePIPVideo.plugin.js
@@ -14,47 +14,10 @@ class RemovePIPVideo {
 		
 		// Start/Stop
 		start () {
-			if (!window.BDFDB) window.BDFDB = {myPlugins:{}};
-			if (window.BDFDB && window.BDFDB.myPlugins && typeof window.BDFDB.myPlugins == "object") window.BDFDB.myPlugins[this.getName()] = this;
-			let libraryScript = document.querySelector("head script#BDFDBLibraryScript");
-			if (!libraryScript || (performance.now() - libraryScript.getAttribute("date")) > 600000) {
-				if (libraryScript) libraryScript.remove();
-				libraryScript = document.createElement("script");
-				libraryScript.setAttribute("id", "BDFDBLibraryScript");
-				libraryScript.setAttribute("type", "text/javascript");
-				libraryScript.setAttribute("src", "https://mwittrien.github.io/BetterDiscordAddons/Plugins/BDFDB.min.js");
-				libraryScript.setAttribute("date", performance.now());
-				libraryScript.addEventListener("load", _ => {this.initialize();});
-				document.head.appendChild(libraryScript);
-			}
-			else if (window.BDFDB && typeof BDFDB === "object" && BDFDB.loaded) this.initialize();
-			this.startTimeout = setTimeout(_ => {
-				try {return this.initialize();}
-				catch (err) {console.error(`%c[${this.getName()}]%c`, "color: #3a71c1; font-weight: 700;", "", "Fatal Error: Could not initiate plugin! " + err);}
-			}, 30000);
 			document.querySelectorAll('.pictureInPicture-3VocJq').forEach(e => e.style.display = 'none');
 		}
 
-		initialize () {
-			if (window.BDFDB && typeof BDFDB === "object" && BDFDB.loaded) {
-				if (this.started) return;
-				BDFDB.PluginUtils.init(this);
-				
-				BDFDB.ModuleUtils.forceAllUpdates(this);
-			}
-			else {
-				console.error(`%c[${this.getName()}]%c`, 'color: #3a71c1; font-weight: 700;', '', 'Fatal Error: Could not load BD functions!');
-			}
-		}
-
 		stop () {
-			if (window.BDFDB && typeof BDFDB === "object" && BDFDB.loaded) {
-				this.stopping = true;
-				
-				BDFDB.ModuleUtils.forceAllUpdates(this);
-
-				BDFDB.PluginUtils.clear(this);
-			}
 			document.querySelectorAll('.pictureInPicture-3VocJq').forEach(e => e.style.display = 'inherit');
 		}
 	}
