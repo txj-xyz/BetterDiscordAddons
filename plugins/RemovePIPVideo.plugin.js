bec47a188c1568c319d99bf03eb53fc4e4fb67aa
0x0b6e48 <jfinelli3@gmail.com>
2020-04-21 15:03:05 -0600

    formatting

--- plugins/RemovePIPVideo.plugin.js
+++ plugins/RemovePIPVideo.plugin.js
@@ -1,23 +1,23 @@
 //META{"name":"RemovePIPVideo"}*//
 class RemovePIPVideo {
+	
+	// Meta Get Name and Information for Plugin.
+	getName() { return "Remove Video PIP"; }
+	getShortName() { return "RemovePIPVideo"; }
+	getDescription() { return "Get rid of the Guild Video chat PIP window when leaving the video chat."; }
+	getVersion() { return "1.0.2"; }
+	getAuthor() { return "TXJ"; }
 
-		// Meta
-		getName() { return "Remove Video PIP"; }
-		getShortName() { return "RemovePIPVideo"; }
-		getDescription() { return "Get rid of the Guild Video chat PIP window when clicking on a text chat."; }
-		getVersion() { return "1.0.2"; }
-		getAuthor() { return "TXJ"; }
-
-		
-		// Load/Unload
-		load() { }
-		
-		// Start/Stop
-		start () {
-			document.querySelectorAll('.pictureInPicture-3VocJq').forEach(e => e.style.display = 'none');
-		}
-
-		stop () {
-			document.querySelectorAll('.pictureInPicture-3VocJq').forEach(e => e.style.display = 'inherit');
-		}
+	
+	// Load/Unload
+	load() { }
+	
+	// Start/Stop
+	start () {
+		document.querySelectorAll('.pictureInPicture-3VocJq').forEach(e => e.style.display = 'none');
 	}
+
+	stop () {
+		document.querySelectorAll('.pictureInPicture-3VocJq').forEach(e => e.style.display = 'inherit');
+	}
+}
